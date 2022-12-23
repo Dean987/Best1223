cbce4163636629cd3914c76c4bf1ed06922c2f5f
Dean987 <dean861031@gmail.com>
2022-12-23 19:20:33 +0800

    version 0: html55

--- index.py
+++ index.py
@@ -33,6 +33,17 @@ for t in meals:
     doc_ref.set(doc)
 
 
+@app.route("/webhook", methods=["POST"])
+def webhook():
+    # build a request object
+    req = request.get_json(force=True)
+    # fetch queryResult from json
+    action =  req.get("queryResult").get("action")
+    msg =  req.get("queryResult").get("queryText")
+    info = "動作：" + action + "； 查詢內容：" + msg
+    return make_response(jsonify({"fulfillmentText": info}))
+
+
 
 
 
