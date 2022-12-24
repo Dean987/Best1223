a73c09b1f6780ef9418ad93f547a38c4e585a8ca
Dean987 <dean861031@gmail.com>
2022-12-24 20:06:49 +0800

    version 0: html5

--- index.py
+++ index.py
@@ -50,12 +50,10 @@ def webhook():
         result = ""
         for doc in docs:
             dict=doc.to_dict()
-            if taste=="全部" :
+            if taste  ==  "全部" :
                 result += "品名：" + dict["name"] + "\n"
                 result += "介紹：" + dict["say"] +"\n\n"
                 result += "網址：" + dict["hypertext"] +"\n\n"
-
-
             elif taste == dict["taste"]:
                 result += "品名：" + dict["name"] + "\n"
                 result += "介紹：" + dict["say"] +"\n\n"
