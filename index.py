ea960da696dd09602d36411dfbc210d36109e3ea
Dean987 <dean861031@gmail.com>
2022-12-25 12:55:19 +0800

    version 0: html5

--- index.py
+++ index.py
@@ -74,7 +74,7 @@ def webhook():
                 result += "介紹：" + dict["say"] +"\n\n"
                 result += "辣度：" + dict["taste"] + "\n"
                 result += "網址：" + dict["hyperlink"] +"\n\n" 
-        if result == "":
+        if result != "" and name:
             result += "抱歉，查無相關條件的產品資訊" 
         
         info += result
